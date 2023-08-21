3ddce465389f8ccd65b4d74c3700c93e1eb6795b
yukihung <cji3u04ruru@gmail.com>
2023-08-21 16:37:52 +0800

    Final_1

--- docker/data.sql
+++ docker/data.sql
@@ -3,7 +3,7 @@
 -- Host: 127.0.0.1    Database: simpleshop
 -- ------------------------------------------------------
 -- Server version	8.0.27
-
+-- test for homework
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
